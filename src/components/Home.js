c565b212b4e3f370c243084f23b85d0d98a4c791
Vishwajeet Shinde <shindevishwajeet434@gmail.com>
2024-10-12 11:55:46 +0530

    home .js changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,20 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Home() {
+  const githubToken = localStorage.getItem('github_token');
+
   const handleLogin = () => {
-    const clientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
-    const redirectUri = `${window.location.origin}/callback`;
-    window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=repo,admin:repo_hook`;
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&scope=repo,admin:repo_hook`;
   };
 
   return (
     <div>
       <h1>GitHub PR Review</h1>
-      <button onClick={handleLogin}>Connect to GitHub</button>
-      {localStorage.getItem('github_token') && (
-        <Link to="/manage-repo">
-          <button>Manage Repository Webhook</button>
-        </Link>
+      {!githubToken ? (
+        <button onClick={handleLogin}>Connect to GitHub</button>
+      ) : (
+        <>
+          <Link to="/manage-repo">
+            <button>Manage Repository Webhook</button>
+          </Link>
+        </>
       )}
     </div>
   );
